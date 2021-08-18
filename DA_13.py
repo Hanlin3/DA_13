fc4a618b36c44de834770587fe7a00a8d001c5f5
Hayden Cheong <hayden@haydencheong.com>
2021-08-19 04:12:32 +0800

    Web scraper beta

--- DA_13.py
+++ DA_13.py
@@ -1,20 +1,12 @@
+import scrapy
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ['http://192.168.1.1/index.html']
-
+    start_urls = ['https://ite.edu.sg/']
     def parse(self, response):
-        xpath_selector = '//img'
-        for x in response.xpath(xpath_selector):
+        css_selector = 'img'
+        for x in response.css(css_selector):
             newsel = '@src'
             yield {
                 'Image Link': x.xpath(newsel).extract_first(),
             }
 
-# To recurse next page
-Page_selector = '.next a ::attr(href)'
-next_page = response.css(Page_selector).extract_first()
-if next_page:
-    yield scrapy.Request(
-        response.urljoin(next_page),
-        callback=self.parse
-    )
